98d7b97d881809e264df09f605c0c44e63c09163
Oron Nadiv <oron@nadiv.us>
2017-08-03 09:16:29 -0700

    Unknown request to warn log level, remove domain container.

--- src/app.js
+++ src/app.js
@@ -1,24 +1,11 @@
-const error = require('debug')('ha:app:error')
-
 const diehard = require('diehard')
-const domain = require('domain')
 const expressInitializer = require('./initializations/express')
 const Motions = require('./db/collections/motions')
 const Promise = require('bluebird')
 const Toggles = require('./db/collections/toggles')
 
-const d = domain.create()
-
-d.on('error', error)
-
-d.run(() => {
-  Promise
-    .resolve(Motions.purge())
-    .then(() => {
-      return Toggles.purge()
-    })
-    .then(() => {
-      return expressInitializer.initialize()
-    })
-    .then(() => diehard.listen({timeout: 5000}))
-})
+Promise
+  .resolve(Motions.purge())
+  .then(() => Toggles.purge())
+  .then(() => expressInitializer.initialize())
+  .then(() => diehard.listen({timeout: 5000}))
